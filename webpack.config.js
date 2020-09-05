c15d5483299cf5bab06fe593ebff8554ac0e9785
Your Name <minh16031994@gmail.com>
2020-09-05 13:50:39 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports={
+    entry:'./app/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename: 'index_bundle.js'
+    },
+    module:{
+        rules: [
+            {test: /\.(js)$/, use:'babel-loader'},
+            {test: /\.css$/, use:['style-loader','css-loader']},
+            {test: /\.scss$/, use:['style-loader','sass-loader']},
+            {test: /\.(png|svg|jpg|gif)$/,use:'file-loader'}
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./app/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
